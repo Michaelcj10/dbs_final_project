033d69434f7ff34b87d3735b321a6d277aebb6a8
michael <michaelcjames10@gmail.com>
2020-11-14 16:19:19 +0000

    work on cypress tests

--- cypress/cypress/integration/examples/notifications/notifications_spec.js
+++ cypress/cypress/integration/examples/notifications/notifications_spec.js
@@ -8,8 +8,17 @@ describe("Notifications", () => {
       response: "fixture:notifications.json",
     }).as("notifications");
 
+    cy.visit("localhost:3000/notifications");
+
     cy.wait("@notifications");
 
-    cy.visit("localhost:3000/notifications");
+    cy.visit("localhost:3000/dashboard");
+
+    cy.get(".ant-badge > .ant-avatar").click();
+
+    cy.get(".ant-list-items > :nth-child(1)");
+    cy.get(".ant-list-items > :nth-child(2)");
+    cy.get(".ant-list-items > :nth-child(3)");
+    cy.get(".ant-list-items > :nth-child(4)");
   });
 });
